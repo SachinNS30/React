e92651ecf8fa38c1063e5e656d96f00337dd34b3
Sachin <146916737+SachinNS30@users.noreply.github.com>
2024-09-06 15:27:20 +0530

    Add files via upload

---
+++ React-Projects-main/Day 1/CE/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function alertMsg()
+{
+  alert('Message from Javascript Alert!');
+  console.log('Message to developer');
+}
+function App() {
+  return (
+    <div>
+      <h1>Let we see the output of JAVASCRIPT</h1>
+      <button onClick={alertMsg}>Click</button>
+    </div>
+  );
+}
+
+export default App;
